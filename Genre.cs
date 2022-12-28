5252184bb8024856ad01cf8ba55fb33016ebbacd
Senior C# <104258857+Mohammad-Goma@users.noreply.github.com>
2022-12-28 23:36:13 +0200

    Add files via upload

---
+++ Genre.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestFullApiFirstProject.Models
+{
+    public class Genre
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+    }
+}
