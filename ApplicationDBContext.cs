f964b9acc90c3a46dc12fc9d86b8bf9a5b394c5a
Senior C# <104258857+Mohammad-Goma@users.noreply.github.com>
2022-12-28 23:35:58 +0200

    Add files via upload

---
+++ ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestFullApiFirstProject.Models;
+
+namespace RestFullApiFirstProject.Data
+{
+
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Genre> Genres { get; set; }
+
+    }
+}
